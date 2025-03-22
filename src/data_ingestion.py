71e0f808dadf8a6d4cd7a4771781cfb3eec9fe61
Moazzam <mdmoazzamali984@gmail.com>
2025-03-22 17:47:26 +0530

    params config for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
-
+import yaml
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -26,6 +26,20 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load the parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters loaded from %s', params_path)
+        return params
+    except FileNotFoundError as e:
+        logger.error('Failed to load parameters: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the parameters: %s', e)
+        raise
 
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
@@ -68,7 +82,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        #test_size = 0.2
         data_path = 'https://raw.githubusercontent.com/alimoazzam123/Datasets/refs/heads/main/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
--- src/model_building.py
+++ src/model_building.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 import logging
 from sklearn.ensemble import RandomForestClassifier
+import yaml
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -27,6 +28,19 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path: str) -> dict:
+    """Load the parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters loaded from %s', params_path)
+        return params
+    except FileNotFoundError as e:
+        logger.error('Failed to load parameters: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the parameters: %s', e)
+        raise
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
@@ -101,7 +115,7 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators': 25, 'random_state': 2}
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
